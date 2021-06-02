3eb8c9bc3e3adb2ce65ccf0bf2085e33607e4687
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2021-06-02 10:47:16 -0400

    added proj files

---
+++ ws2-1/salReport.cpp
@@ -0,0 +1,18 @@
+// Workshop 2: 
+// Version: 1.0
+// Date: 2021/5/28
+// Author: Fardad Soleimanloo
+// Description:
+// This file tests the part 1 section of your workshop
+#define _CRT_SECURE_NO_WARNINGS
+// Do not modify your code
+/////////////////////////////////////////////
+#include "Employee.h"
+using namespace sdds;
+int main() {
+   if (load()) {
+      display();
+   }
+   deallocateMemory();
+   return 0;
+}
